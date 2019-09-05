f1bc191bca19eff8c8954be89e3db8d48a2932d3
rgr2228 <raul.gomezr@udea.edu.co>
2019-09-05 15:26:54 -0500

    Tests listos

--- src/ConvertRoman.java
+++ src/ConvertRoman.java
@@ -1,8 +1,16 @@
 public class ConvertRoman {
+	int one;
+	int five;
+	int ten;
+	int fifty;
 	
 	public int romanToDecimal(String romanNumber) {
 		int decimal = 0;
 		int lastNumb = 0;
+		one=0;
+		five=0;
+		ten=0;
+		fifty = 0;
 		String romanNumeral = romanNumber.toUpperCase();
 		for (int i = romanNumeral.length() - 1; i >= 0; i--){
 			char romanChar = romanNumeral.charAt(i);
@@ -14,22 +22,54 @@ public class ConvertRoman {
 			case 'L':
 				decimal = operateDecimal(50, lastNumb, decimal);
 				lastNumb = 50;
+				fifty= fifty + 1;
+				if(fifty>1){
+					return 0;
+				}
 				break;
 			case 'X':
 				decimal = operateDecimal(10, lastNumb, decimal);
 				lastNumb = 10;
+				ten= ten + 1;
+				if(ten>3){
+					return 0;
+				}
 				break;
 			case 'V':
-				decimal = operateDecimal(5, lastNumb, decimal);
-				lastNumb = 5;
+				if(lastNumb==100 || lastNumb==50 ||lastNumb==10){
+					return 0;
+				}
+				else {
+					decimal = operateDecimal(5, lastNumb, decimal);
+					lastNumb = 5;
+					five= five + 1;
+					if(five>1){
+						return 0;
+					}
+				}
 				break;
 			case 'I':
-				decimal = operateDecimal(1, lastNumb, decimal);
-				lastNumb = 1;
+				if(lastNumb==100 || lastNumb==50){
+					return 0;
+				}
+				else
+				{
+					decimal = operateDecimal(1, lastNumb, decimal);
+					lastNumb = 1;
+					one= one + 1;
+					if(one>3) {
+						return 0;
+					}
+				}
 				break;
 			}
 		}
-		return decimal;
+		if(decimal>100) {
+			return 0;
+		}
+		else {
+			return decimal;
+		}
 	}
 
 	public int operateDecimal(int decimal, int lastNumber, int lastDecimal){
--- test/ConvertRomanTest.java
+++ test/ConvertRomanTest.java
@@ -2,12 +2,106 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ConvertRomanTest {
-
+	// I debe equivaler a 1
 	@Test
-	public void equalFive() {
+	public void iEqualsToOne() {
 		ConvertRoman convertRoman = new ConvertRoman();
-		int result = convertRoman.romanToDecimal("vi");
-		assertEquals(6,result);
+		int result = convertRoman.romanToDecimal("I");
+		assertEquals(1, result);
 	}
-	
+
+	// V debe equivaler a 5
+	@Test
+	public void vEqualsToFive() {
+		ConvertRoman convertRoman = new ConvertRoman();
+		int result = convertRoman.romanToDecimal("V");
+		assertEquals(5, result);
+	}
+
+	// X debe equivaler a 10
+	@Test
+	public void xEqualsToTen() {
+		ConvertRoman convertRoman = new ConvertRoman();
+		int result = convertRoman.romanToDecimal("X");
+		assertEquals(10, result);
+	}
+
+	// L debe equivaler a 50
+	@Test
+	public void lEqualsToFifty() {
+		ConvertRoman convertRoman = new ConvertRoman();
+		int result = convertRoman.romanToDecimal("L");
+		assertEquals(50, result);
+	}
+
+	// C debe equivaler a 100
+	@Test
+	public void cEqualsToOneHundred() {
+		ConvertRoman convertRoman = new ConvertRoman();
+		int result = convertRoman.romanToDecimal("C");
+		assertEquals(100, result);
+	}
+
+	// V no puede estar a la izquierda de un número más grande
+	// Da 0 si el resultado es un número no válido
+	@Test
+	public void vBeforeGreater() {
+		ConvertRoman convertRoman = new ConvertRoman();
+		int result = convertRoman.romanToDecimal("VC");
+		assertEquals(0, result);
+	}
+
+	// V no puede aparecer más de 1 vez
+	// Da 0 si el resultado es un número no válido
+	@Test
+	public void vTwoTimes() {
+		ConvertRoman convertRoman = new ConvertRoman();
+		int result = convertRoman.romanToDecimal("VIV");
+		assertEquals(0, result);
+	}
+
+	// L no puede aparecer más de 1 vez
+	// Da 0 si el resultado es un número no válido
+	@Test
+	public void lTwoTimes() {
+		ConvertRoman convertRoman = new ConvertRoman();
+		int result = convertRoman.romanToDecimal("LIL");
+		assertEquals(0, result);
+	}
+
+	// I sólo puede aparecer máximo 3 veces
+	// Da 0 si el resultado es un número no válido
+	@Test
+	public void iMoreThanThreeTimes() {
+		ConvertRoman convertRoman = new ConvertRoman();
+		int result = convertRoman.romanToDecimal("IIII");
+		assertEquals(0, result);
+	}
+
+	// I sólo puede aparecer a la izquierda de V o X
+	// Da 0 si el resultado es un número no válido
+	@Test
+	public void iBeforeLOrC() {
+		ConvertRoman convertRoman = new ConvertRoman();
+		int result = convertRoman.romanToDecimal("IL");
+		assertEquals(0, result);
+	}
+
+	// X sólo puede aparecer máximo 3 veces
+	// Da 0 si el resultado es un número no válido
+	@Test
+	public void xMoreThanThreeTimes() {
+		ConvertRoman convertRoman = new ConvertRoman();
+		int result = convertRoman.romanToDecimal("XXXX");
+		assertEquals(0, result);
+	}
+
+	// Los números válidos van del 1 al 100
+	// Da 0 si el resultado es un número no válido
+	@Test
+	public void greaterThanOneHundred() {
+		ConvertRoman convertRoman = new ConvertRoman();
+		int result = convertRoman.romanToDecimal("CI");
+		assertEquals(0, result);
+	}
 }
